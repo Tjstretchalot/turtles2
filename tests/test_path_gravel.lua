17ec9fe389a1782c7856690e6a9b78ae96dcb6d4
Timothy Moore <mtimothy984@gmail.com>
2021-04-17 11:00:10 -0700

    Fix misundestanding set_path

--- tests/test_path_gravel.lua
+++ tests/test_path_gravel.lua
@@ -45,14 +45,14 @@ local function main()
     local store = init_store()
     local mem = {}
     local rdest = vector.new(0, 0, 2)
-    local succ, path = path_utils.set_path(store, mem, rdest, WORLD, true, true)
+    local succ = path_utils.set_path(store, mem, rdest, WORLD, true, true)
     if not succ then error('failed to find path') end
-    mem.current_path = path
-    mem.current_path_ind = 1
 
     textutils.slowPrint(
         string.format('currently at %s', textutils.serialize(store.raw.move_state.position))
     )
+    textutils.slowPrint('facing %s', store.raw.move_state.dir)
+
     textutils.slowPrint('Path:')
     for i, ele in ipairs(mem.current_path) do
         textutils.slowPrint(string.format('%2d: %s', i, ele))
