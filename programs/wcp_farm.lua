d8c36a9155657fc07fd768c78aad3150d482d1c7
Timothy Moore <mtimothy984@gmail.com>
2021-04-17 14:37:20 -0700

    Support taller wcp farms

--- programs/wcp_farm.lua
+++ programs/wcp_farm.lua
@@ -15,6 +15,13 @@
 --     Same deal, 3 empty height (including seed) for carrot farm, above it is
 --     the potato farm
 --     The turtle moves between farms by going up/down above the fuel chest
+--
+-- This will generate "farm.config", which will have the following configuration
+-- options (textutils.serialize style):
+-- - count [number] (default 1): Must be a positive integer. If higher than one,
+--   then this assumes theres that many wheat, carrot, and potato farms. FOr example,
+--   if count is 2, then there are 6 9x9 farms (wheat, carrot, potato, wheat, carrot,
+--   potato). Does not change the chest layout.
 
 
 package.path = '../?.lua;turtles2/?.lua'
@@ -23,61 +30,139 @@ local farm = require('utils/farm')
 local startup = require('utils/startup')
 local inv = require('utils/inv')
 
+local DEFAULT_SETTINGS = {
+    count = 1,
+}
+
+local function load_settings()
+    local filename = 'farm.config'
+    local settings = state.deep_copy(DEFAULT_SETTINGS)
+
+    if fs.exists(filename) then
+        local h = fs.open(filename, 'r')
+        local txt = h.readAll()
+        h.close()
+
+        local file_settings = textutils.unserialize(txt)
+        for key, _ in pairs(DEFAULT_SETTINGS) do
+            local val = file_settings[key]
+            if val ~= nil then
+                settings[key] = val
+            end
+        end
+    end
+
+    local h = fs.open(filename, 'w')
+    h.write(textutils.serialize(settings))
+    h.close()
+
+    return settings
+end
+
+local function init_seeds(settings)
+    local function init_wheat()
+        return {
+            has_seed = true,
+            chest = vector.new(-2, -1, 0),
+            pred = inv.new_pred_by_name('minecraft:wheat_seeds'),
+            checker = function(data)
+                return (
+                    data.name ~= 'minecraft:wheat'
+                    or data.state.age >= 7)
+            end
+        }
+    end
+
+    local function init_carrots()
+        return {
+            has_seed = true,
+            chest = vector.new(2, -1, -2),
+            pred = inv.new_pred_by_name('minecraft:carrot'),
+            checker = function(data)
+                return (
+                    data.name ~= 'minecraft:carrots'
+                    or data.state.age >= 7)
+            end
+        }
+    end
+
+    local function init_potatoes()
+        return {
+            has_seed = true,
+            chest = vector.new(-2, -1, -2),
+            pred = inv.new_pred_by_name('minecraft:potato'),
+            checker = function(data)
+                return (
+                    data.name ~= 'minecraft:potatoes'
+                    or data.state.age >= 7)
+            end
+        }
+    end
+
+    return {
+        init_wheat(),
+        init_carrots(),
+        init_potatoes()
+    }
+end
+
+local function init_farms(settings)
+    local function init_farm(seed, layer)
+        return {
+            seed = seed,
+            time_between_checks = 30,
+            farm_presets.offset_values(preset.farm, vector.new(0, 4 * layer, 1))
+        }
+    end
+
+    local nseeds = 3
+    local res = {}
+    for i = 1, settings.count do
+        for j = 1, nseeds do
+            res.append(init_farm(j, (i - 1) * nseeds + j - 1))
+        end
+    end
+    return res
+end
+
+local function init_world(settings)
+    local arr = {}
+    for layer=1, settings.count * 3 do
+        local y = (layer-1) * 4
+        table.insert(arr, farm_presets.world_rect(
+            vector.new(-4, y, 2),
+            vector.new(4, y, 10)
+        ))
+        table.insert(arr, {
+            [tostring(vector.new(0, y, 1))] = true,
+        })
+    end
+    table.insert(
+        arr, farm_presets.world_rect( -- chests
+            vector.new(-2, 0, -2),
+            vector.new(2, 0, 0)
+        )
+    )
+    table.insert(
+        arr,
+        farm_presets.world_rect( -- connection
+            vector.new(0, 0, 0),
+            vector.new(0, ((settings.count * 3) - 1) * 4, 0)
+        )
+    )
+
+    return farm_presets.union(arr)
+end
+
 local function main()
     startup.inject('programs/wcp_farm.lua')
 
     local preset = farm_presets.SIMPLE
+    local settings = load_settings()
 
     farm.main(
-        { -- seeds
-            { -- wheat
-                has_seed = true,
-                chest = vector.new(-2, -1, 0),
-                pred = inv.new_pred_by_name('minecraft:wheat_seeds'),
-                checker = function(data)
-                    return (
-                        data.name ~= 'minecraft:wheat'
-                        or data.state.age >= 7)
-                end
-            },
-            { -- carrot
-                has_seed = true,
-                chest = vector.new(2, -1, -2),
-                pred = inv.new_pred_by_name('minecraft:carrot'),
-                checker = function(data)
-                    return (
-                        data.name ~= 'minecraft:carrots'
-                        or data.state.age >= 7)
-                end
-            },
-            { -- potato
-                has_seed = true,
-                chest = vector.new(-2, -1, -2),
-                pred = inv.new_pred_by_name('minecraft:potato'),
-                checker = function(data)
-                    return (
-                        data.name ~= 'minecraft:potatoes'
-                        or data.state.age >= 7)
-                end
-            }
-        },
-        { -- farms
-            { -- 9x9 wheat
-                seed = 1,
-                time_between_checks = 30, -- minutes
-                locs = farm_presets.offset_values(preset.farm, vector.new(0, 0, 1))
-            },
-            { -- 9x9 carrots
-                seed = 2,
-                time_between_checks = 30, -- minutes
-                locs = farm_presets.offset_values(preset.farm, vector.new(0, 4, 1))
-            },
-            { -- 9x9 potatoes
-                seed = 3,
-                time_between_checks = 30, -- minutes
-                locs = farm_presets.offset_values(preset.farm, vector.new(0, 8, 1))
-            }
-        },
+        init_seeds(settings),
+        init_farms(settings),
         { -- specific chests
             { -- seeds
                 pred = inv.new_pred_by_name('minecraft:wheat_seeds'),
@@ -98,32 +183,7 @@ local function main()
         },
         vector.new(0, -1, 0), -- fuel chest
         vector.new(0, -1, -2), -- excess chest
-        farm_presets.union( -- world
-            {
-                farm_presets.world_rect( -- wheat
-                    vector.new(-4, 0, 2),
-                    vector.new(4, 0, 10)),
-                farm_presets.world_rect( -- carrots
-                    vector.new(-4, 4, 2),
-                    vector.new(4, 4, 10)),
-                farm_presets.world_rect( -- potatoes
-                    vector.new(-4, 8, 2),
-                    vector.new(4, 8, 10)),
-                farm_presets.world_rect( -- chests
-                    vector.new(-2, 0, -2),
-                    vector.new(2, 0, 0)
-                ),
-                farm_presets.world_rect( -- connection
-                    vector.new(0, 0, 0),
-                    vector.new(0, 8, 0)
-                ),
-                { -- extra connecting blocks
-                    [tostring(vector.new(0, 0, 1))] = true,
-                    [tostring(vector.new(0, 4, 1))] = true,
-                    [tostring(vector.new(0, 8, 1))] = true
-                }
-            }
-        )
+        init_world(settings)
     )
 end
 
