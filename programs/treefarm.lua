06b399e5b17e823975fa6a7e2ed411885a205dd1
Timothy Moore <mtimothy984@gmail.com>
2021-04-17 08:43:27 -0700

    Add new birch tree support

--- programs/treefarm.lua
+++ programs/treefarm.lua
@@ -33,11 +33,14 @@
 
 local SAPLING_NAMES = {
     ['minecraft:sapling'] = true,
+    ['minecraft:birch_sapling'] = true,
     ['IC2:blockRubSapling'] = true
 }
 
 local GROWTHS = {
     ['minecraft:log'] = true,
+    ['minecraft:birch_log'] = true,
+    ['minecraft:birch_leaves'] = true,
     ['minecraft:log2'] = true,
     ['IC2:blockRubWood'] = true,
     ['IC2:blockRubLeaves'] = true,
@@ -364,7 +367,7 @@ local OBJECTIVE_TICKERS = {
             local fn_ind = constants.MOVE_TO_FN_IND[
                 mem.current_path[#mem.current_path]]
             local inspect_fn = constants.INSPECT_FN[fn_ind]
-            select_count_saplings()
+            select_count_saplings(go bac)
             local succ, data = turtle[inspect_fn]()
             if succ then
                 if SAPLING_NAMES[data.name] then
